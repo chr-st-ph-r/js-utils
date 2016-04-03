15c8c8b5ec1374a46fbc9ee1734b374224af0bc4
Chris Carrassi <ccarrassi@gmail.com>
2016-04-03 08:16:16 -0700

    added some more functions

--- utils.js
+++ utils.js
@@ -11,3 +11,23 @@ function _(tag, father, id, _class) {
   father.appendChild( new_elem );
   return new_elem;
 };
+
+function assert(condition, message) {
+	if (!condition) {
+		throw message || "Assert failed";
+	}
+}
+
+function shuffle(array) {
+    // http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+    var counter = array.length;
+    while (counter > 0) {
+        var index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        var temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }  
+}
